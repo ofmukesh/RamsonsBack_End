cac5873c99f128e12f4b2a19d344b57c806f4e4f
x1mukesh <of.mukesh@gmail.com>
2022-11-25 17:35:43 +0530

    update

--- company/views.py
+++ company/views.py
@@ -8,7 +8,7 @@ from django.db.models import F
 
 class ComapnyView(viewsets.ModelViewSet):
     serializer_class = CompanySerializer
-    queryset = company.objects.order_by('-update_on')
+    queryset = company.objects.filter(active=True).order_by('-update_on')
     filterset_fields = {'company_name': [
         "in"], 'person_name': ['contains'], 'phone_no': ['contains'], 'created_on': ['range']}
 
